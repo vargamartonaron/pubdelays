fa357a73c14c3abfc3eb799f24deef270f665b2c
martonaronvarga <martonaronvarga@gmail.com>
2023-12-04 20:04:22 +0100

    groundhog_date

--- src/process_data.R
+++ src/process_data.R
@@ -4,7 +4,7 @@ library(groundhog)
 
 packages <- c("dplyr", "jsonlite", "fuzzyjoin", "tidyr", "readr", "lubridate", "stringr")
 
-groundhog.library(packages, "2023-12-04")
+groundhog.library(packages, "2023-12-02")
 
 # import json and csv data
 
