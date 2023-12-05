16954a9f0ebba95fd183cc44c497f38e580934f5
martonaronvarga <martonaronvarga@gmail.com>
2023-12-05 16:47:12 +0100

    trial

--- src/plotting.R
+++ src/plotting.R
@@ -10,18 +10,18 @@ articles <- readr::read_tsv('/users/usumusu/pubdelays/journal_articles.tsv')
 acceptance_data <- articles |>
   dplyr::group_by(article_date) |>
   dplyr::filter(lubridate::year(article_date) >= 2016) |>
-  dplyr::reframe(delay=median(acceptance_delay, na.rm=T))
+  dplyr::reframe(delay=median(acceptance_delay, na.rm=T)) |>
+  tidyr::drop_na(delay)
 
   
-acceptance_plot <- ggplot(acceptance_data, aes(x = article_date, y = delay, group = 1)) +
-  #geom_smooth(method = "loess", se = TRUE, span = 1, color = "black", linewidth = 1.2) +
-  geom_point(alpha = 1/ 2000) +
-  scale_x_date(date_breaks = '1 year', date_labels = '%Y')
+acceptance_plot <- ggplot(acceptance_data, aes(x = article_date, y = delay)) +
+  geom_point(alpha = 1/10) +
+  scale_x_date(date_breaks = '1 year', date_labels = '%Y') +
   labs(y = "Acceptance delay (days)", x = "Year") +
-  theme_apa() +
   theme(axis.text.x = element_text(size = 16, family = "Times", hjust = 1),
         axis.text.y = element_text(size = 16, family = "Times"),
         axis.title = element_text(size = 22, family = "Times")) +
-  ylim(0, 1000)
+  theme_apa()
 
   ggsave('acceptance_plot.pdf', width = 7, height = 7, scale = 0.9, dpi = 100)
+  
