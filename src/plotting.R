3f5c4325426e902d2088335a075a3d6e6168bf92
martonaronvarga <martonaronvarga@gmail.com>
2023-12-05 16:51:58 +0100

    almost there

--- src/plotting.R
+++ src/plotting.R
@@ -10,12 +10,12 @@ articles <- readr::read_tsv('/users/usumusu/pubdelays/journal_articles.tsv')
 acceptance_data <- articles |>
   dplyr::group_by(article_date) |>
   dplyr::filter(lubridate::year(article_date) >= 2016) |>
-  dplyr::reframe(delay=median(acceptance_delay, na.rm=T)) |>
+  dplyr::reframe(delay=mean(acceptance_delay, na.rm=T)) |>
   tidyr::drop_na(delay)
 
   
 acceptance_plot <- ggplot(acceptance_data, aes(x = article_date, y = delay)) +
-  geom_point(alpha = 1/10) +
+  geom_point(alpha = 1/5) +
   scale_x_date(date_breaks = '1 year', date_labels = '%Y') +
   labs(y = "Acceptance delay (days)", x = "Year") +
   theme(axis.text.x = element_text(size = 16, family = "Times", hjust = 1),
